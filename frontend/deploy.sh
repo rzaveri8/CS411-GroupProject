50719535b038e3258f37854b8981e3a983290107
Manuja DeSilva <mdesilva@bu.edu>
2019-03-28 17:28:24 -0400

    Build for prod auto attempt

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ng build --prod
+echo "Build complete"
 git add .
-git commit -m "build for prod"
-ssh -i "411-aws-key.pem" ubuntu@ec2-52-14-17-113.us-east-2.compute.amazonaws.com 'bash' << git pull
+git commit -m "Build for prod auto attempt"
+git push
+echo "Pushed to Git repo"
+ssh -i "411-aws-key.pem" ubuntu@ec2-52-14-17-113.us-east-2.compute.amazonaws.com bash << pullFromGit.sh
+echo "Done"
