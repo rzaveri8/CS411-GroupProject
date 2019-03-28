c785413bddb2a2111620c4f362ff8211ff697dcb
Manuja DeSilva <mdesilva@bu.edu>
2019-03-28 17:20:29 -0400

    build for prod

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,4 +3,4 @@
 ng build --prod
 git add .
 git commit -m "build for prod"
-ssh -i "411-aws-key.pem" ubuntu@ec2-52-14-17-113.us-east-2.compute.amazonaws.com 'bash' << git push
+ssh -i "411-aws-key.pem" ubuntu@ec2-52-14-17-113.us-east-2.compute.amazonaws.com 'bash' << git pull
