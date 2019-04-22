20b9300c9b1546c31c7a54ef30bba1557098f8b5
Laura Joy Erb <laurajoy@bu.edu>
2019-04-22 13:08:38 -0400

    forces user to input missing industry

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router'
 import { allSettled } from 'q';
 
+declare var $: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +13,8 @@ import { allSettled } from 'q';
 export class DashboardComponent implements OnInit {
 
   profile: any =[];
+  rawIndustry: string;
+
   constructor(public httpClient: HttpClient, private router: Router) { }
 
   getProfile() {
@@ -20,8 +23,42 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  parsePosition(position_string){
+    var raw = position_string;
+    var string_contents = raw.split(" ");
+
+    //check if only one word passed into position
+    if(string_contents.length == 1){ //capitalize word and return
+        var parsed = raw.charAt(0).toUpperCase() + raw.slice(1);
+        //extract the first character of this string and "capitalize" it, and then leave the rest of the string as is and append it to a new string
+        //console.log(position_string);
+        return parsed;
+    }
+
+    //else run loop to capitalize each word
+    for(var i=0;i<string_contents.length;i++){
+      var raw_curr_string = string_contents[i];
+      var parsed_string = raw_curr_string.charAt(0).toUpperCase() + raw_curr_string.slice(1);
+      string_contents[i] = parsed_string;
+
+    }
+
+    //add space in between strings
+    var parsed = string_contents.join(" ");
+    return parsed;
+  }
+
+
+  updateIndustry()  {
+    this.profile.industry = this.parsePosition(this.rawIndustry);
+  }
+
   ngOnInit() {
     this.getProfile();
+    if (this.profile.industry == undefined)
+    {
+      $("#myModal").modal();
+    }
   };
 
 }
