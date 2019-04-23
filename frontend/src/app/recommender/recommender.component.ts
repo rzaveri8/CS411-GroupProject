6e0907ae49a1ee3a5a5956818d8f6794ad6d1be6
Laura Joy Erb <laurajoy@bu.edu>
2019-04-22 23:45:08 -0400

    added loading and cards

--- frontend/src/app/recommender/recommender.component.ts
+++ frontend/src/app/recommender/recommender.component.ts
@@ -13,11 +13,16 @@ export class RecommenderComponent implements OnInit {
   jobs: any;
   industry: string;
   error: string;
+  loading: boolean;
+
   ngOnInit() {
+    this.loading = true;
+    this.error = "";
     this.http.get("/api/jobs").subscribe((res)=> {
       this.response = res;
       this.jobs = this.response.data;
       this.industry = this.response.industry;
+      this.loading = false;
       if(this.response.error){
         this.error = this.response.error;
       }
